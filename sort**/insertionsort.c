b45b01cc708caf9a8a223f9b2bf3448541d0b4aa
0x11semprez <traoresemprez@icloud.com>
2026-02-20 14:27:04 +0100

    feat: start insertionSort

---
+++ sort**/insertionsort.c
@@ -0,0 +1,14 @@
+static void insertionSort(int *a, int n) {
+  int i;
+  for (i = 1; i < n; i++) {
+    int j;
+    int key = a[i]
+    int j = i - 1; 
+
+    while (j >= 0 && a[j] > key) {
+      a[j + 1] = a[j];
+      j--;
+    }
+    a[j + 1] = key;
+  }
+}
