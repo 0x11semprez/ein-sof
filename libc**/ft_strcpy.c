699ab315322c22902e722564a6b932230da8f082
0x11semprez <traoresemprez@icloud.com>
2026-02-27 10:58:49 +0100

    feat(add): add fr strcpy

---
+++ libc**/ft_strcpy.c
@@ -0,0 +1,17 @@
+char *ft_strcpy(char *dest, char *src) {
+  int i = 0;
+  while (src[i] != '\0') {
+    dest[i] = src[i];
+    i++;
+  }
+  dest[i] = '\0';
+  return dest;
+}
+
+int main(void) {
+  char buffer[20];
+  ft_strcpy(buffer, "Ghost");
+  write(1, buffer, 5);
+  write(1, '\n', 5);
+  return 0;
+}
